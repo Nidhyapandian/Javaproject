3475a69888b0b474b8034d435990305c96511977
Nidhya R <163541752+Nidhyapandian@users.noreply.github.com>
2025-02-20 04:05:06 +0530

    Update main.tf

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -97,7 +97,7 @@ resource "aws_instance" "my_ec2_instance1" {
       
       # Install Jenkins 
       # REF: https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo \https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
       "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
       "sudo yum upgrade",
       # Add required dependencies for the jenkins package
